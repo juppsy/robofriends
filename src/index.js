69c25bc387cf93b95b81b04df67913eb4040bbfa
Sebastian Latkolic <latkolicsebastian@gmail.com>
2021-01-11 16:28:10 +0200

    redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./containers/App";
-import "./index.css";
-import "tachyons";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import App from './containers/App';
+import './index.css';
+import 'tachyons';
+import reportWebVitals from './reportWebVitals';
+import { searchRobots, requestRobots } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
